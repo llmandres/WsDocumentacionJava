66d3a83cd423a122f4e9b4171b168c607bf8f6a1
MarkDelsius <marcosdelsaz2005@gmail.com>
2024-04-11 13:14:50 +0200

    añadida la documentacion de las 2 clases

--- Documentacion_14/src/documentacion/Jugador.java
+++ Documentacion_14/src/documentacion/Jugador.java
@@ -1,11 +1,26 @@
 package documentacion;
 
+/**
+ * La clase jugador contiene al objeto jugador cuyos atributos son
+ * el numero de dorsal, numero de tarjetas amarillas, y numero de tarjetas rojas.
+ * Tiene dos metodos, uno para ingresar el numero de dorsal del jugador, y otro para 
+ * saber si el jugador esta expulsado.
+ */
 public class Jugador {
 
+	/**
+	 * Atributo de tipo int que representa el numero de dorsal del jugador
+	 */
 	private int dorsal;
 
+	/**
+	 * Atributo de tipo int que representa el numero de tarjetas amarillas del jugador.
+	 */
 	private int numeroTarjetasAmarillas;
 
+	/**
+	 * Atributo de tipo int que representa el numero de tarjetas rojas del jugador
+	 */
 	private int numeroTarjetasRojas;
 
 	public int getDorsal() {
@@ -33,7 +48,15 @@ public class Jugador {
 	}
 
 	
-	
+	/**
+	 * Metodo para ingresar el numero de dorsal del jugador, si el numero ingresado 
+	 * por parametro es igual o mayor que 1 y menos o igual que 30, el dorsal sera 
+	 * valido y se establecera correctamente, en caso de que la norma anterior no se respete,
+	 * el dorsal se igualara a "-1"
+	 * 
+	 * @param dorsal numero ingresado por parametro para establecer el numero de dorsal comprendido entre
+	 * el 1 y el 30 incluidos estos 2 , para que sea valido, en caso de no ser valido, el dorsal sera igual a "-1"
+	 */
 	public void ponerDorsal(int dorsal) {
 
 		if (dorsal >= 1 && dorsal <= 30) {
@@ -47,7 +70,17 @@ public class Jugador {
 		}
 
 	}
-
+	
+	
+	/**
+	 * metodo que establece si el jugador esta expulsado o no, se establece por defecto que el jugador
+	 * no esta expulsado, por lo que el atributo "expulsado" del metodo, se iguala a "false" significando esto
+	 * que el jugador no esta expulsado, si el atributo "numeroTarjetasAmarillas" del jugador es igual a 2,
+	 * el atributo "expulsado" se igualara a true, significando que el jugador esta expulsado, si el atributo
+	 * "numeroTarjetasRojas" del jugador es igual a 1, el atributo "expulsado" tambien se igualara a true, por lo
+	 * que el jugador tambien estara expulsado en este caso. El metodo finalmente devuelve el atributo "expulsado"
+	 * @return Devuelve el atributo "expulsado" significando "true" que el jugador esta expulsado, y "false" que no lo esta.
+	 */
 	public boolean estaExpulsado() {
 
 		boolean expulsado = false;
--- Documentacion_14/src/documentacion/Soldado.java
+++ Documentacion_14/src/documentacion/Soldado.java
@@ -1,13 +1,29 @@
 package documentacion;
 
+/**
+ * La clase Soldado contiene el objeto soldado, cuyos atributos son
+ * el atributo estaMuerto, que representa si el soldado esta o no muerto
+ * y el atributo numeroBalas, que indica el numero de balas del soldado
+ * Y encontraremos metodos para saber si el soldado puede o no disparar y el
+ * metodo correpondiente para disparar en caso de que pueda.
+ */
 public class Soldado {
+	
+	/**
+	*Atributo de valor booleano, que representa si el soldado esta muerto ("true") o si esta vivo ("false")
+	*/
 	private boolean estaMuerto;
 
+	/**
+	*Atributo cuyo tipo de valor es int y representa el numero de balas de un soldado
+	*/
 	private int numeroBalas;
 
+	
 	public boolean isEstaMuerto() {
 		return estaMuerto;
 	}
+
 
 	public void setEstaMuerto(boolean estaMuerto) {
 		this.estaMuerto = estaMuerto;
@@ -22,7 +38,11 @@ public class Soldado {
 	}
 
 	
-	
+	/**
+	*Metodo que devuelve true o false dependiendo de si el soldado puede o no disparar, en caso
+	*de que el soldado tenga un numero superior a 0 en el atributo "numeroBalas", el metodo devolvera
+	*"true" en caso de tener 0 o menos, el metodo devolvera "false"
+	*/
 	public boolean puedeDisparar() {
 
 		if (this.numeroBalas > 0) {
@@ -35,6 +55,12 @@ public class Soldado {
 
 	}
 
+	/**
+	 * Metodo que lleva a cabo la accion de disparar a un soldado ingresado por parametro
+	 * al disparar, el numero de balas de el soldado de la clase disminuye en 1 unidad, y el
+	 * soldado ingresado por parametro, moriria, igualando su atributo "estaMuerto" a true.
+	 * @param sol Soldado ingresado por parametro que quieres que muera
+	 */
 	public void disparar(Soldado sol) {
 
 		this.numeroBalas--;
