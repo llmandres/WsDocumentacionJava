6bb2018529e16375633e6a9b870de3da6d9bed9f
yolmi <andresgarciasampayo@gmail.com>
2024-04-11 12:40:56 +0200

    Creacion de clases y getters and setters

---
+++ Documentacion_14/src/documentacion/Soldado.java
@@ -0,0 +1,46 @@
+package documentacion;
+
+public class Soldado {
+	private boolean estaMuerto;
+
+	private int numeroBalas;
+
+	public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
+
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
+
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
+
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
+
+	
+	
+	public boolean puedeDisparar() {
+
+		if (this.numeroBalas > 0) {
+
+			return true;
+
+		}
+
+		return false;
+
+	}
+
+	public void disparar(Soldado sol) {
+
+		this.numeroBalas--;
+
+		sol.estaMuerto = true;
+
+	}
+
+}
